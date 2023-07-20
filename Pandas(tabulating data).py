b5776756c2144c24d8f3620ed85e73599b3a784b
Trevin Rodrigo <131873760+Trevin007@users.noreply.github.com>
2023-07-20 09:05:30 +0530

    Add files via upload

---
+++ Pandas(tabulating data).py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[30]:
+
+
+import pandas as pd
+
+record1=pd.Series({"Name":"Trevin",
+                "Year":'21',
+                "School":"St.Josephs College",
+                "University:"University of Colombo"})
+record1
+
+import pandas as pd
+
+record1 = pd.Series({
+    "Name": "Trevin",
+    "Age": '21',
+    "School": "St. Joseph's College",
+    "University": "University of Colombo"
+})
+record1
+
+
+# In[31]:
+
+
+record2 = pd.Series({
+    "Name": "Seniya",
+    "Age": '23',
+    "School": "St. Joseph's College",
+    "University": "SLIIT"
+})
+
+record2
+
+
+# In[32]:
+
+
+record3 = pd.Series({
+    "Name": "Kavishka",
+    "Age": '26',
+    "School": "St. Joseph's College",
+    "University": "NSBM"
+})
+record3
+
+
+# In[33]:
+
+
+df=pd.DataFrame([record1,record2,record3],index=['Youngest','Middle','Eldest'])
+df
+
+
+# In[34]:
+
+
+df.head(1)
+
+
+# In[36]:
+
+
+df.head(2)
+
+
+# In[ ]:
+
+
+
+
